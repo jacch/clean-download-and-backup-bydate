c7af1d0d3ccf6ba1fdaf3c47f647b4acab45f3d5
jacch <jacch.tw@gmail.com>
2019-11-17 02:25:39 +0800

    create clean

--- cleanDownloads.py
+++ cleanDownloads.py
@@ -1,8 +1,53 @@
+#coding:utf-8
 import shutil
 import os
 import time
+import datetime
+now = datetime.datetime.now()
 
 #Getting into the Downloads folder.
-os.chdir(os.path.expanduser("~") + os.sep + "Downloads")
+downloaddir=os.path.expanduser("~") + os.sep + "Downloads"+ os.sep
+List = os.listdir(downloaddir)
+
+downloaddir1=os.path.expanduser("~") + os.sep + "下載"+ os.sep
+List1 = os.listdir(downloaddir1)
+ 
+
 
 #check dir exist 
+def DirectoriesCreate(DirPath):
+    if not os.path.isdir(DirPath): 
+        os.mkdir (DirPath)
+        
+
+def CreateDirs(Path):
+    DirPath=os.path.expanduser("~") + os.sep + Path
+    DirectoriesCreate(DirPath)
+        
+    DirPath_Y=os.path.expanduser("~") + os.sep + Path+ os.sep+str(now.year) 
+    DirectoriesCreate(DirPath_Y)
+    
+    DirPath_M=os.path.expanduser("~") + os.sep + Path+ os.sep+str(now.year)+ os.sep+str(now.month) 
+    DirectoriesCreate(DirPath_M)
+    return DirPath_M+ os.sep
+
+    
+backup_path=CreateDirs("Download_backup1")
+ 
+def move (L1,P1,backup_path):
+    newdate=str(now.year)+str(now.month)+str(now.day)
+    for lists in L1:
+        sourcefile=(P1+lists)
+        targetfile=(backup_path+lists)
+        #print sourcefile+targetfile
+        if not os.path.isdir(sourcefile): 
+            if not os.path.isfile(targetfile):
+                #print (targetfile) 
+                 shutil.move(sourcefile,targetfile)
+            else:
+                shutil.move(sourcefile,targetfile+newdate)
+
+
+ 
+move(List1,downloaddir1,backup_path)
+move(List,downloaddir,backup_path)
